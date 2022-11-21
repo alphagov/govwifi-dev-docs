a5e236c2a26a4ce11af51e788e81bf674aff4914
Jim Narey <james.narey@digital.cabinet-office.gov.uk>
2022-11-21 16:42:20 +0000

    Fix YAML parse errors caused by upgrade to Ruby 3.1.2
    To overcome errors thrown by the latest version of the YAML
    parser it has been necessary to overload a method in the
    GovUK Tech Docs template/gem.
    
    The team responsible have been contacted in order to see
    whether we can get this changes in the gem. If so, we will
    be able to revert this element of the commit but will have to
    retain the other part, the quoted dates.

--- config.rb
+++ config.rb
@@ -1,3 +1,13 @@
 require 'govuk_tech_docs'
 
 GovukTechDocs.configure(self)
+
+helpers do
+  def format_date(date)
+    if date.is_a?(Date)
+      date.strftime("%-e %B %Y")
+    else
+      Date.parse(date).strftime("%-e %B %Y")
+    end
+  end
+end
